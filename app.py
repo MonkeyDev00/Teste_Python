c2bf60085dcc7d33ad5f9218e3a01a2c4f355c27
MonkeyDev00 <lucasjstos03@gmail.com>
2024-09-08 14:39:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+/// 
+Esse sistema deve ser capaz de fazed a soma entre dois números.
+///
+
+#definindo uma função de soma
+def soma (a,b):
+    valor = a,b 
+    
+#local de entrada de dados
+print('Digite um valor: ')
+valor1 = int(input());  #entrada do primeiro valor
+
+print('Digite outro valor: ');
+valor2 = int(input());  #entrada do segundo valor
+
+print('A soma entre os dois valores é: ')   
+total = soma(valor1,valor2):
+print(soma); #saída do valor da soma dos dois valores
