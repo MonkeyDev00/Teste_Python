9f51e1d7ac1b378e4064387ca02c2248566bcc19
MonkeyDev00 <lucasjstos03@gmail.com>
2024-09-09 18:03:35 -0300

    app1.py

---
+++ app1.py
@@ -0,0 +1,8 @@
+#fazendo a soma entre dois números 
+
+print('POR FAVOR DIGITE UM NÚMERO: ');
+valor1 = int(input());
+print('POR FAVOR DIGITE UM SEGUNDO NÚMERO: ');
+valor2 = int(input());
+total = (valor1 + valor2);
+print('A SOMA ENTRE OS DOIS VALORES É EQUIVALENTE Á: ', total);
